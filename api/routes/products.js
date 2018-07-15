e36a49f5d8db2bd4499491db3aca988b052c631f
aga <aga@adeptforms.com>
2018-07-15 19:43:14 +0700

    Create Upload Middleware

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,35 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
 const checkAuth = require('../middleware/auth');
-
+const uploadFile = require("../middleware/uploads");
 const ProductsController = require('../controllers/products');
-
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, './uploads/');
-    },
-    filename: function(req, file, cb) {
-        cb(null, Math.random() + '_' + file.originalname);
-    }
-});
-const fileFilter = (req, file, cb) => {
-    // Reject a file
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-        cb(null, true)
-    } else {
-        cb(null, false)
-    }
-}
-const upload = multer({
-    storage: storage,
-    limits: 1024 * 1024 * 5,
-    fileFilter: fileFilter
-});
 
 router.get('/', ProductsController.products_get_all);
 
-router.post('/save', checkAuth, upload.single('productImage'), ProductsController.products_create_product);
+router.post('/save', checkAuth, uploadFile.upload.single('productImage'), ProductsController.products_create_product);
 
 router.get('/get/:productId', ProductsController.products_get_product);
 
